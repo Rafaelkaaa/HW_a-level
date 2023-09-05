07e2d589d27949e3ff1c646bc844526424d6fdfc
Vlada <proskurina.vlada@gmail.com>
2023-09-05 22:56:39 +0300

    Refactoring

--- hw_3_crud/src/main/java/org/example/service/RaiderCrudService.java
+++ hw_3_crud/src/main/java/org/example/service/RaiderCrudService.java
@@ -1,7 +1,5 @@
 package org.example.service;
 
-import lombok.AccessLevel;
-import lombok.experimental.FieldDefaults;
 import org.example.craft.collections.CraftArrayList;
 import org.example.entity.Raider;
 
