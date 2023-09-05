92b47226262c15ec53aa54cde57721c204b55947
Vlada <proskurina.vlada@gmail.com>
2023-09-05 22:49:49 +0300

    Add Main

---
+++ hw_3_crud/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.example.controller.RaiderController;
+
+public class Main {
+    public static void main(String[] args) {
+        new RaiderController().mainMenu();
+    }
+}
