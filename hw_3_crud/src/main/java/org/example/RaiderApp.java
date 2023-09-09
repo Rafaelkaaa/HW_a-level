aa1a7edc652470b19fc0db7327d2e5fcdfa7a393
IvanovNikita <nikita.ivanov@bearg.net>
2023-09-09 21:36:05 +0300

    Refactoring

--- hw_3_crud/src/main/java/org/example/RaiderApp.java
+++ hw_3_crud/src/main/java/org/example/RaiderApp.java
@@ -2,7 +2,7 @@ package org.example;
 
 import org.example.controller.RaiderController;
 
-public class Main {
+public class RaiderApp {
     public static void main(String[] args) {
         new RaiderController().mainMenu();
     }
